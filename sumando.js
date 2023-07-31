0bbbc87ac3716b58eedda2e603efbe15f66eecfb
Marializ17 <maria.lizcanos0225@gmail.com>
2023-07-31 18:05:56 -0500

    se añade la carpeta misión con el ejercicio 1

---
+++ sumando.js
@@ -0,0 +1,11 @@
+function suma(num1, num2) {
+    return num1 + num2;
+  }
+
+console.log(suma(10, 20)); // 30
+
+console.log(suma(10, 10)); // 20
+
+console.log(suma(40, 60)); // 100
+
+console.log(suma(50, 200)); // 250
