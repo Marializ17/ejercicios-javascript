0a82e4e4af26ec245e0ff9ff14c38502d81fe076
Marializ17 <maria.lizcanos0225@gmail.com>
2023-07-31 17:57:58 -0500

    se añade corrección

--- saludando.js
+++ saludando.js
@@ -1,8 +1,8 @@
-let time = 15;
+let time = 11.9;
 
-if( time <= 12){
+if( time < 12){
     console.log("Buenos Días")
-} else if ( time > 12 && time <= 18){
+} else if ( time >= 12 && time <= 18){
     console.log("Buenas Tardes")
 } else{
     console.log("Buenas Noches")
